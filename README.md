4b1d027fd789d081b372a6fa9ba5bb30b5e86d82
Christopher Boyd <boyd108@live.com>
2021-02-03 00:55:31 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# google-swap-in-place
+ A Chrome plugiin for stopping Chrome account swapping from creating a new tab and instead changing the current tab to the destination account
---
+++ content-scripts/content.js
@@ -0,0 +1,23 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // $("head").prepend(
+  //   `<style>
+  //      .slide-image {
+  //         height: auto;
+  //         width: 100vw;
+  //       }
+  //     </style> `
+  // );
+  // $("body").prepend(
+  //   `<img  src="${request.url}" id="${request.imageDivId}" 
+  //          class="slide-image" /> `
+  // );
+  // $(`#${request.imageDivId}`).click(function() {
+  //   $(`#${request.imageDivId}`).remove(`#${request.imageDivId}`);
+  // });
+  // sendResponse({ fromcontent: "This message is from content.js" });
+});
+
+$(document).bind("DOMSubtreeModified", function(evt) {
+  $('header > div > div > div > div > a').attr('target', '_self'); // Keep
+  $('header > div > div > div > div > a').attr('target', '_self'); // Gmail
+})
